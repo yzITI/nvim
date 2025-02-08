c136f432f6b8adeae558f8fe82ebef096a877f3b
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2025-02-08 16:36:28 -0600

    Move extras config

--- lua/plugins/extras.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.coding.mini-comment" },
-  { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-  { import = "lazyvim.plugins.extras.editor.mini-move" },
-  { import = "lazyvim.plugins.extras.editor.refactoring" },
-  { import = "lazyvim.plugins.extras.editor.dial" },
-}
